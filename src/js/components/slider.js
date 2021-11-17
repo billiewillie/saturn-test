d83f6c19fecd105230cb3f2bd76a85f832b31a23
Vadim <vadimbelinovich@gmail.com>
2021-11-17 03:00:21 +0300

    edited

---
+++ src/js/components/slider.js
@@ -0,0 +1,13 @@
+import Flickity from "flickity";
+import { query } from "./query";
+const services = query(".services-list");
+
+const slider = new Flickity(services, {
+	draggable: true,
+	freeScroll: true,
+	contain: true,
+	watchCSS: true,
+	cellAlign: "left",
+});
+
+export default slider;
